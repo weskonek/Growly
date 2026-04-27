980e68dee6e6eb83f85029aa4074db87ab4fa3e5
weskonek <weskonek@gmail.com>
2026-04-27 22:35:14 +0700

    refactor: migrate childRouter from riverpod_annotation to standard Provider implementation

--- apps/child_app/lib/core/router/child_router.dart
+++ apps/child_app/lib/core/router/child_router.dart
@@ -1,16 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../features/launcher/presentation/pages/child_launcher_page.dart';
 import '../../features/learning/presentation/pages/learning_hub_page.dart';
 import '../../features/ai_tutor/presentation/pages/ai_tutor_page.dart';
 import '../../features/rewards/presentation/pages/rewards_page.dart';
 
-part 'child_router.g.dart';
-
-@riverpod
-GoRouter childRouter(ChildRouterRef ref) {
+/// Router provider for child app
+final childRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: '/launcher',
     routes: [
@@ -36,4 +33,4 @@ GoRouter childRouter(ChildRouterRef ref) {
       ),
     ],
   );
-}
+});
